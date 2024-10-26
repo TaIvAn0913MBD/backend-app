9b8b9699b46c0e4d1a8751b89ed4fd8cdb33643a
24HP6298 24HP6298 <24HP6298@G305-CC0244.local>
2024-10-26 13:58:57 +0800

    innit

--- index.js
+++
@@ -1,5 +0,0 @@
-[
-  { age: 13, name: "Taivan", school: "Oros_3" },
-  { age: 13, name: "Tuvshin", school: "Oros_3" },
-  { age: 13, name: "Tuugii", school: "Oros_3" },
-];
--- users.js
+++ users.js
@@ -1,10 +1,3 @@
-const usersData = [
-  { age: 13, name: "Taivan", school: "Oros_3", id: "1" },
-  { age: 13, name: "Tuvshin", school: "Oros_3", id: "2" },
-  { age: 13, name: "Tuugii", school: "Oros_3", id: "3" },
-];
-
-const { countReset } = require("console");
 // const service = http.createServer((req, res) => {
 //   const url = req.url;
 
@@ -21,17 +14,115 @@ const { countReset } = require("console");
 // });
 // service.listen(8090, console.log("success"));
 
+const http = require("http");
+const fs = require("fs");
+const JSONDATA = fs.readFileSync("index.json");
+let data;
+data = JSON.parse(JSONDATA);
+
+const service = http.createServer((req, res) => {
+  const method = req.method;
+  const url = req.url;
+  res.setHeader("Content-Type", "application/Json");
+  GetUser(method, req, res, url, data);
+
+  PostUser(method, req, res, data);
+
+  DeleteUser(method, req, res, data);
+});
+service.listen(8090, console.log("success"));
+
+const PostUser = (method, req, res) => {
+  if (method === "POST") {
+    let body = "";
+    req.on("data", (buff) => {
+      body += buff;
+    });
+    console.log(body);
+    req.on("end", () => {
+      const praseData = JSON.parse(body);
+
+      const addNewUser = {
+        id: data.length + 1,
+        ...praseData,
+      };
+
+      data.push(addNewUser);
+
+      fs.writeFileSync("index.json", JSON.stringify(data), (err) => {
+        console.log(err);
+      });
+      console.log(praseData);
+      res.write(JSON.stringify({ message: "done" }));
+      res.end();
+    });
+  }
+};
+
+const DeleteUser = (method, req, res) => {
+  if (method === "DELETE") {
+    let body = "";
+    req.on("data", (buff) => {
+      body += buff;
+    });
+    res.end("end", () => {
+      const praseData = JSON.parse(body);
+      const removeId = Number(praseData.id);
+      console.log(removeId);
+      const result = data.filter((item) => {
+        return Number(item.id) !== removeId;
+      });
+      console.log(result);
+
+      fs.writeFileSync("index.json", JSON.stringify(result), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
+
+const GetUser = (method, req, res, url) => {
+  if (method === "GET") {
+    if (url.startsWith("/users?id=")) {
+      res.statusCode = 200;
+      const UserId = url.split("=")[1];
+      const users = data.find((hereglegch) => {
+        return hereglegch.id === UserId;
+      });
+
+      if (users) {
+        res.write(JSON.stringify(users));
+      } else {
+        res.write(JSON.stringify({ error: "no message found" }));
+      }
+
+      res.end();
+    } else {
+      res.statusCode = 200;
+      res.write(JSON.stringify(data));
+      res.end();
+    }
+  }
+};
+
 // const http = require("http");
+// const url = require("url");
+// const fs = require("fs");
+// const JSONDATA = fs.readFileSync("index.json");
+// let data;
+// data = JSON.parse(JSONDATA);
 
 // const service = http.createServer((req, res) => {
-//   const url = req.url;
+//   const q = url.parse(req.url, true);
 //   res.setHeader("Content-Type", "application/Json");
-//   if (url.startsWith("/users?id=")) {
+//   if (q.pathname === "/users") {
 //     res.statusCode = 200;
-//     const UserId = url.split("=")[1];
+//     const qData = q.query;
+//     const UserKey = Object.keys(qData)[0];
+//     const UserValue = qData[UserKey];
 
 //     const users = usersData.find((hereglegch) => {
-//       return hereglegch.id === UserId;
+//       return hereglegch[UserKey] === UserValue;
 //     });
 //     if (users) {
 //       res.write(JSON.stringify(users));
@@ -42,40 +133,8 @@ const { countReset } = require("console");
 //     res.end();
 //   } else {
 //     res.statusCode = 200;
-//     res.write(JSON.stringify(usersData));
-//     console.log("hi");
+//     res.write(JSON.stringify({ Error: "no such website" }));
 //     res.end();
 //   }
 // });
 // service.listen(8090, console.log("success"));
-const http = require("http");
-const url = require("url");
-
-const service = http.createServer((req, res) => {
-  const q = url.parse(req.url, true);
-  res.setHeader("Content-Type", "application/Json");
-  if (q.pathname === "/users") {
-    res.statusCode = 200;
-    const qData = q.query;
-    const UserKey = Object.keys(qData)[0];
-    console.log(typeof UserKey);
-    const UserValue = qData[UserKey];
-    console.log(UserKey);
-    console.log(qData.UserKey);
-    const users = usersData.find((hereglegch) => {
-      return hereglegch.UserKey === UserKey;
-    });
-    if (users) {
-      res.write(JSON.stringify(users));
-    } else {
-      res.write(JSON.stringify({ Error: "no message found" }));
-    }
-
-    res.end();
-  } else {
-    res.statusCode = 200;
-    res.write(JSON.stringify({ Error: "no such website" }));
-    res.end();
-  }
-});
-service.listen(8090, console.log("success"));
